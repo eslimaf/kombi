d47610bd3893c5d5a56f19c5423518fca2ed3c5a
Evandro Lima <eslimaf@gmail.com>
2017-04-19 11:20:38 -0300

    Add Javadoc

--- kombi/src/main/java/com/eslimaf/kombi/Garage.java
+++ kombi/src/main/java/com/eslimaf/kombi/Garage.java
@@ -1,14 +1,25 @@
 package com.eslimaf.kombi;
 
+import android.support.annotation.NonNull;
+
 import java.util.HashMap;
 
+/**
+ * The type Garage.
+ */
 public class Garage {
     private static Garage sInstance;
+    @NonNull
     private HashMap<String, RxKombi> mInstanceMap = new HashMap<>();
 
     private Garage() {
     }
 
+    /**
+     * Gets instance.
+     *
+     * @return the instance of Garage
+     */
     public static synchronized Garage getsInstance() {
         if (sInstance == null) {
             sInstance = new Garage();
@@ -16,17 +27,38 @@ public class Garage {
         return sInstance;
     }
 
+    /**
+     * Ride kombi rx kombi.
+     *
+     * @param <T>          the type parameter
+     * @param licensePlate identify the bus
+     * @return the rx kombi
+     */
+    @SuppressWarnings("unchecked")
     public <T> RxKombi<T> rideKombi(String licensePlate) {
         if (!mInstanceMap.containsKey(licensePlate)) {
             mInstanceMap.put(licensePlate, RxKombi.create());
         }
-        return mInstanceMap.get(licensePlate);
+
+        if (mInstanceMap.get(licensePlate) != null) {
+            return mInstanceMap.get(licensePlate);
+        } else {
+            throw new RuntimeException("License plate not found");
+        }
     }
 
+    /**
+     * Remove a kombi from the garage.
+     *
+     * @param licensePlate the license plate
+     */
     public void sellKombi(String licensePlate) {
         mInstanceMap.remove(licensePlate);
     }
 
+    /**
+     * Clear the garage removing all kombis.
+     */
     public void clearGarage() {
         mInstanceMap.clear();
     }
--- kombi/src/main/java/com/eslimaf/kombi/RxKombi.java
+++ kombi/src/main/java/com/eslimaf/kombi/RxKombi.java
@@ -3,26 +3,62 @@ package com.eslimaf.kombi;
 import io.reactivex.Observable;
 import io.reactivex.subjects.PublishSubject;
 
+/**
+ * The type Kombi with a PublishSubject.
+ *
+ * @param <T> the type parameter
+ */
 public class RxKombi<T> {
     private PublishSubject<T> mBus = PublishSubject.create();
+    private String mLicensePlate;
 
-    private RxKombi() {
-        //Avoid new instance
+    public RxKombi(String licensePlate) {
+        mLicensePlate = licensePlate;
     }
 
+    /**
+     * Create rx kombi.
+     *
+     * @param <T> the type parameter
+     * @return the rx kombi
+     */
     public static <T> RxKombi<T> create() {
-        return new RxKombi<>();
+        return new RxKombi<>("");
     }
 
+    /**
+     * Send.
+     *
+     * @param t the t
+     */
     public void send(T t) {
         mBus.onNext(t);
     }
 
+    /**
+     * To observable observable.
+     *
+     * @return the observable
+     */
     public Observable<T> toObservable() {
         return mBus;
     }
 
+    /**
+     * Has observers boolean.
+     *
+     * @return the boolean
+     */
     public boolean hasObservers() {
         return mBus.hasObservers();
+    }
+
+    /**
+     * Gets license plate.
+     *
+     * @return the license plate
+     */
+    public String getLicensePlate() {
+        return mLicensePlate;
     }
 }
